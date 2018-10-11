0df39ac050e44c0625e043fa04e5a586a7a436c3
Gerardo <gherrer1@binghamton.edu>
2018-10-11 19:43:09 -0400

    Bootstrapped project. Ready to write and test code

---
+++ compile.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const solc = require('solc');
+
+const pathToSolFiles = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+try {
+    const contents = fs.readFileSync(pathToSolFiles, 'utf8');
+    const compiledSource = solc.compile(contents, 1);
+    module.exports = compiledSource.contracts[':Inbox'];
+} catch(e) {
+    console.log(e.message);
+}
